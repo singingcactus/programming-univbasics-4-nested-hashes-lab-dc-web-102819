d5f2e2c9041d19b173380607c6d74a983de2d667
Matt Bechtel <bechma02@gmail.com>
2019-10-20 20:27:44 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -74,7 +74,6 @@ def adding_matz
           :known_for => "Ruby",
           :languages => ["LISP, "C"]
         }
-
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
